3c820f7b09ad7bc20482dce225d507135213c18b
Jessica Dash <167637926+jlbdash@users.noreply.github.com>
2024-05-21 13:55:59 -0400

    Update Update.js

--- ui/src/Update.js
+++ ui/src/Update.js
@@ -20,7 +20,7 @@ function Updater(props) {
     return (
         <>
             Update a Movie!
-            <form name="updatemovie" id="updatemovie" method="POST" action="http://localhost:4000/update/"  >
+            <form name="updatemovie" id="updatemovie" method="POST" action="http://localhost:4000/movie/{movie.id}"  >
                 <input type="hidden" name="id" id="id" value={movie.id} />
                 <label>Title: <input type="text" name="name" id="name" defaultValue={movie.name} /> </label> <br />
                 <label>Genre: <input type="text" name="genre" id="genre" defaultValue={movie.genre} /></label> <br />
