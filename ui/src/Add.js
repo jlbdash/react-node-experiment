834ffb1083936bafb7ab3a309542c3a32ea6c009
Jessica Dash <167637926+jlbdash@users.noreply.github.com>
2024-05-21 12:29:40 -0400

    Update Add.js

--- ui/src/Add.js
+++ ui/src/Add.js
@@ -1,14 +1,25 @@
 import "./App.css";
 
-function Movies() {
+function Movies(props) {
 
-
+const onSubmit = (event) => {
+    // prevent the form from doing it's normal job, we want to do everything async
+    event.preventDefault();
+    // make a fetch request
+    fetch("http://localhost:4000/movies", {
+      method: "POST",
+      body: JSON.stringify(movie),
+    }).then(() => {
+      props.onChange();
+      setMovie(defaultMovie);
+    });
+  };
 
     return (
         <>
             Add to the List!
 
-            <form name="handlemovie" id="handlemovie" method="POST" action="http://localhost:4000/movies">
+            <form name="handlemovie" id="handlemovie" onSubmit={onSubmit}>
                 <label>Title: <input type="text" name="name" id="name" required /> </label> <br />
                 <label>Genre: <input type="text" name="genre" id="genre" required /></label> <br />
                 <label>Release Date: <input type="text" name="date" id="date" required /></label> <br />
